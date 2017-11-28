e651216321300139815d18cb0d5830c2e038211a
= <=>
2017-11-28 12:04:17 +0000

    Add basic user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+    validates :name, presence:true, length: {maximum: 50}
+    EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence:true, length: {maximum: 255}, format: { with: EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
 end
