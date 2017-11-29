67753bf9a189fa9e00e2e0f74dd419195ec3ea26
= <=>
2017-11-29 11:45:54 +0000

    Add user email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+    before_save { self.email = email.downcase }
     validates :name, presence:true, length: {maximum: 50}
     EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence:true, length: {maximum: 255}, format: { with: EMAIL_REGEX }, uniqueness: { case_sensitive: false }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127115050) do
+ActiveRecord::Schema.define(version: 20171128123641) do
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
@@ -26,5 +26,7 @@ ActiveRecord::Schema.define(version: 20171127115050) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
