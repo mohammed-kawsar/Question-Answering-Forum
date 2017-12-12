247dd82e31fa98011b11069f2c602d8fd439d852
Mohammed Kawsar <k1631491@kcl.ac.uk>
2017-12-12 13:32:13 +0000

    Restore routes from master

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  resources :users 
+  get 'sessions/new'
+
+  resources :users
   resources :questions do 
     resources :comments
   end
 
-  root 'questions#index', as: 'home'
+  root 'questions#index'
+  
+  get  '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
 
 
 end
