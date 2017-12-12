47cec49e90261e27de3b17d709e2690cb5ce6495
Jakub <k1502754@kcl.ac.uk>
2017-12-12 15:18:32 +0000

    Added Published at, and fixed question preview

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212140014) do
+ActiveRecord::Schema.define(version: 20171212141457) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
