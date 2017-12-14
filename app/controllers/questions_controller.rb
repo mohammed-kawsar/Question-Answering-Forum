f572d3300dfcab117d888a62b99cd6565f5cca1f
Wonjoon Seol <wonjoon.seol@kcl.ac.uk>
2017-12-14 17:45:47 +0000

    add edit, delete question functionality

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
-    before_action :logged_in_user, only: [:create, :destroy]
-
+    before_action :logged_in_user, only: [:create]
+    before_action :current_user, only: [:edit, :update, :destroy]
 
     def index
         @questions = Question.all
@@ -21,9 +21,28 @@ class QuestionsController < ApplicationController
     end
     
     def destroy
+        @question = Question.find(params[:id])
         
+        @question.destory
+        # redirect_to request.referrer || root_url
+        redirect_to questions_url
     end
 
+    def updated
+        @question = Question.find(params[:id])
+
+        if @question.update(question_params)
+          flash[:success] = "You have created a new question successfully."
+          redirect_to questions_path
+        else
+          render 'edit'
+        end        
+    end
+    
+    def edit
+        @question = Question.find(params[:id])
+    end
+    
     def show
         @question = Question.find(params[:id])
     end
