9cde418adf76ba2e5d748f46435f3272023deb32
Jakub <k1502754@kcl.ac.uk>
2017-12-14 17:51:09 +0000

    if guest wants to post a comment, it redirects him to login page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
-before_action :correct_user, only: [:edit, :update, :destroy]
+before_action :correct_user, only: [ :destroy]
+before_action :logged_in_user, only: [:create]
 
 def correct_user
     @comment = Comment.find_by(id: params[:id])
